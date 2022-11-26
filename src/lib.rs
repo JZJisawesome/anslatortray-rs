80656d66d2aefc12b6fbf1424e0bea26fd08d08c
John Zacarias Jekel <JZJisawesome123@gmail.com>
2022-11-26 16:24:57 -0500

    Implement translate_word

--- src/lib.rs
+++ src/lib.rs
@@ -46,9 +46,9 @@ pub fn translate(english: &str) -> String {
 }
 
 ///Translates a single word string into Pig Latin!
-pub fn translate_word(english_word: &str) -> String {
+pub fn translate_word(english_word: &str) -> Option<String> {
     if english_word.is_empty() {
-        return "".to_string();
+        return None;
     }
 
     let mut iterator = english_word.chars();
@@ -57,27 +57,43 @@ pub fn translate_word(english_word: &str) -> String {
     if is_vowel(first_char) {
         let mut pig_latin_string = english_word.to_string();
         pig_latin_string.push_str(VOWEL_START_STYLE);
-        return pig_latin_string;
+        return Some(pig_latin_string);
     }
 
-    //TODO what if there are no vowels
+    let mut first_char_was_upper = first_char.is_ascii_uppercase();
 
-    let mut starting_part_of_word: String = first_char.to_string();
+    let mut vowel_encountered: bool = false;
+    let mut starting_part_of_word: String = first_char.to_ascii_lowercase().to_string();
+    let mut pig_latin_string: String = "".to_string();
     for letter in iterator {
-        if is_vowel(letter) {
-            break;
+        if !letter.is_alphabetic() {
+            return None;
+        }
+
+        if vowel_encountered {
+            pig_latin_string.push(letter);
         } else {
-            starting_part_of_word.push(letter);
+            if is_vowel(letter) {
+                if first_char_was_upper {
+                    pig_latin_string.push(letter.to_ascii_uppercase())
+                } else {
+                    pig_latin_string.push(letter.to_ascii_lowercase())
+                }
+                vowel_encountered = true;
+            } else {
+                starting_part_of_word.push(letter.to_ascii_lowercase());
+            }
         }
     }
 
-    let mut pig_latin_string: String = "".to_string();
-    for letter in iterator {
-
+    if !vowel_encountered {
+        return None;
     }
-    pig_latin_string.push_str(&starting_part_of_word);
 
-    todo!();
+    pig_latin_string.push_str(&starting_part_of_word);
+    pig_latin_string.push_str("ay");
+
+    return Some(pig_latin_string);
 }
 
 ///Returns true if the paramter is a letter and a vowel, and false otherwise
@@ -101,5 +117,28 @@ mod tests {
         for letter in "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".chars() {
             assert!(!is_vowel(letter));
         }
+    }
+
+    #[test]
+    fn test_translate_word() {
+        assert_eq!(translate_word("Hello").unwrap(), "Ellohay".to_string());
+        assert_eq!(translate_word("World").unwrap(), "Orldway".to_string());
+        assert_eq!(translate_word("This").unwrap(), "Isthay".to_string());
+        assert_eq!(translate_word("is").unwrap(), "is".to_string() + &VOWEL_START_STYLE.to_string());
+        assert_eq!(translate_word("a").unwrap(), "a".to_string() + &VOWEL_START_STYLE.to_string());
+        assert_eq!(translate_word("test").unwrap(), "esttay".to_string());
+        assert_eq!(translate_word("of").unwrap(), "of".to_string() + &VOWEL_START_STYLE.to_string());
+        assert_eq!(translate_word("the").unwrap(), "ethay".to_string());
+        assert_eq!(translate_word("function").unwrap(), "unctionfay".to_string());
+        assert_eq!(translate_word("translate").unwrap(), "anslatetray".to_string());
+        assert_eq!(translate_word("word").unwrap(), "ordway".to_string());
+        assert_eq!(translate_word("I").unwrap(), "I".to_string() + &VOWEL_START_STYLE.to_string());
+        assert_eq!(translate_word("Love").unwrap(), "Ovelay".to_string());
+        assert_eq!(translate_word("Pig").unwrap(), "Igpay".to_string());
+        assert_eq!(translate_word("Latin").unwrap(), "Atinlay".to_string());
+        assert!(matches!(translate_word("bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"), None));//No vowels
+        assert!(matches!(translate_word(""), None));//No letters at all
+        assert!(matches!(translate_word("Multiple Words"), None));
+        assert!(matches!(translate_word("wordwithpunctuation!"), None));
     }
 }
